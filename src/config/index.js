a73d1e07f18e8dde4e2fa621a7625b51c4d3a9bd
shahzaibalam <shahzaibalam231@gmail.com>
2026-07-24 07:36:46 +0500

    folderpath:changes

---
+++ src/config/index.js
@@ -0,0 +1,47 @@
+const config = {
+  redis: {
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+    },
+  },
+  database: {
+    path: "./database.yml", // The database path.
+  },
+  development: {
+    enabled: false, // If true, the bot will register all application commands to a specific guild (not globally).
+    guildId: "",
+  },
+  commands: {
+    prefix: "!", // For message commands, prefix is required. This can be changed by a database.
+    message_commands: true, // If true, the bot will allow users to use message (or prefix) commands.
+    application_commands: {
+      chat_input: true, // If true, the bot will allow users to use chat input (or slash) commands.
+      user_context: true, // If true, the bot will allow users to use user context menu commands.
+      message_context: true, // If true, the bot will allow users to use message context menu commands.
+    },
+  },
+  users: {
+    ownerId: "", // The bot owner ID, which is you.
+    developers: [""], // The bot developers, remember to include your account ID with the other account IDs.
+  },
+  messages: {
+    // Messages configuration for application commands and message commands handler.
+    NOT_BOT_OWNER:
+      "You do not have the permission to run this command because you're not the owner of me!",
+    NOT_BOT_DEVELOPER:
+      "You do not have the permission to run this command because you're not a developer of me!",
+    NOT_GUILD_OWNER:
+      "You do not have the permission to run this command because you\re not the guild owner!",
+    CHANNEL_NOT_NSFW: "You cannot run this command in a non-NSFW channel!",
+    MISSING_PERMISSIONS:
+      "You do not have the permission to run this command, missing permissions.",
+    COMPONENT_NOT_PUBLIC: "You are not the author of this button!",
+    GUILD_COOLDOWN:
+      "You are currently in cooldown, you have the ability to re-use this command again in \`%cooldown%s\`.",
+  },
+};
+
+module.exports = config;
